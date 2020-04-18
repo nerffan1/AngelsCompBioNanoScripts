c136c811e5887592fc9e5225fc8c8dfb7acf0d45
nerffan1 <33079598+nerffan1@users.noreply.github.com>
2020-04-18 03:50:07 -0700

    Update AVG_STRUCTS.tcl

--- AVG_STRUCTS.tcl
+++ AVG_STRUCTS.tcl
@@ -2,21 +2,27 @@
 #--------------------------------------
 #This code seeks to find the average structure at
 #various intervals in a trajectory.
-#MAKE SURE TO Source, or load, the trajectory_smooth.tcl script
+#MAKE SURE TO Source, or load, the trajectory_smooth.tcl script, along this script, in VMD.
+# Arguments:
+#   molid - The VMD Molecule ID
+#   windown - The window number, or the number of frames to average by
+#   prefixname - A prefix to attach to the output pdb files (e.g. 1kf1_9501-10000ns_ave_struct)
+#Last Update: 04/09/20 at 09:54pm
 
-proc Get_avg_pdbs {molid nstructs prefixname} {
+proc Get_avg_pdbs {molid windown prefixname} {
     set TotalFrames [molinfo $molid get numframes]
-    if {$TotalFrames % $nstructs != 0} {
-        puts "Your frames divided by your structures must have 0 remainder."
-        return 0
+    if {$TotalFrames % $windown != 0} {
+        puts "Your frames divided by your structures has no 0 remainder."
+        puts "There'll be frames that are not averaged"
     }
-    set FramesPS [expr $TotalFrames/$nstructs]
+    set nstructs [expr $TotalFrames/$windown]
+    puts "The number of structures is $nstructs"
     set sel [atomselect $molid "nucleic"]
 
     #Loop through the frames and create files for each
     for {set i 0} {$i < $nstructs} {incr i} {
-        set BEG [expr $i*$FramesPS]
-        set END [expr $BEG + $FramesPS - 1]
+        set BEG [expr $i*$windown]
+        set END [expr $BEG + $windown - 1]
         avg_position $sel $prefixname\_$i.pdb beg $BEG end $END writesel selonly
     }
 }
