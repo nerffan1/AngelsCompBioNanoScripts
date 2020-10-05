bb2d3ea9da5f676f2d067a88d4ff59f475915d3b
nerffan1 <33079598+nerffan1@users.noreply.github.com>
2020-10-04 18:45:35 -0700

    CreateTI Update
    This small update makes this program slightly more usable. The Reconverging section has been updated so that it works in a directory holding Input and Output directories. Some old code could still be changed however. I'm hoping to add more functionality in the near future.

--- CreateTI2_1.sh
+++ CreateTI2_1.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #This program works for the submission of QCHEM input in the USC HPC.
-#It's meant to Chiaka Onukwugha
+#It has several functions to submit files to the HPC
 
 #Function Description:
 function CreateFromTags () {
+cd Input/
 for tag in $@
-do 
-   ls -v *$tag* >> TIList
+do
+	ls -v *$tag* >> TILIST
 done
-
-sed -i 's/_input.inp//' TIList
-
+mv TILIST ..
+cd ..
+sed -i 's/_input.inp//' TILIST
 num=0
-
-for file in $(cat TIList)
+for file in $(cat TILIST)
 do 
    cp base.slurm qc$num.slurm
    sed -i "s/inpo/$file/" qc$num.slurm
@@ -21,17 +21,18 @@ do
    sbatch qc$num.slurm
    (( num++ ))
 done
-rm TIList
+rm -f TILIST
 }
 
 #Function Description:
 function Reconvergence () {
-echo "Type in the file with the list of tags to reconverge:"
+echo "Make sure to perform this function in the directory holding Input and Output directories!"
+echo "Type in the file with the list of clean tags to reconverge:"
 read Clean
 
 echo Your current directory is $(pwd)   
 
-echo "Where are the original input files located in relation to current directory?"
+echo "Where are the original input files located in relation to current directory? (include '/') "
 read OgLoc
 
 #Find the location of the input files, in relation to your curent directory, and 
@@ -40,34 +41,58 @@ read OgLoc
 
 num=1
 
+#Makes a directory called ReconvergeFiles 
+echo "A directory called 'ReconvergeFiles' will be created if not found"
+mkdir -p ReconvergeFiles
+cp fix.txt ReconvergeFiles
+echo "The input files to be reconverged will be copied from your Input directory to Reconverge Files"
+#Ask user if he wishes to coninue
+echo "Do you wish to continue?"
+read cont
+if [[ "$cont" != "y" ]]
+then
+	exit 1
+fi 
+#Loop through clean input list. Copy input files 
 for tag in $(cat $Clean)
 do
-	echo Copying ${OgLoc}${tag}_input.inp
-	cp ${OgLoc}${tag}_input.inp .
+	echo Copying ${OgLoc}${tag}_input.inp to ReconvergeFiles
+	cp ${OgLoc}${tag}_input.inp ReconvergeFiles
+	cd ReconvergeFiles
 	sed '43i scf_guess = read' ${tag}_input.inp > ${tag}.mid  
 	sed -e '35r fix.txt' ${tag}.mid > ${tag}.inp
-
+	rm ${tag}.mid
 #The following section 
 	cp base.slurm qc$num.slurm
-   	sed -i "s/inpo/$tag.inp/" qc$num.slurm
-   	sed -i "s/oup/$tag.out/" qc$num.slurm
-	sbatch qc$num.slurm
+   	SetUpBatch $tag $num
    	(( num++ ))	
 done 
 
 }
 
+#This should be ran in the output section.
 function CreateCleanList(){
 sed -e 's/.out//g' IncompleteList > NEWLISTCLEAN
-        mkdir NEWINPUT
+}
+#Description: Rename base.slurm
+#Parameters: (1) Tag, (2) Slurm Batch Number
+function SetUpBatch(){
+cp base.slurm qc$2.slurm
+sed -i "s/inpo/$1/" qc$2.slurm
+sed -i "s/oup/$1.out/" qc$2.slurm
+sbatch qc$2.slurm
+ 
 }
 
 function RangeInput(){
 num=0
+cd Input/
 for tag in $(eval echo {$1..$2..$3})
 do
 	ls -v *$tag* >> TILIST
 done
+mv TILIST ..
+cd ..
 sed -i 's/_input.inp//' TILIST
 
 for file in $(cat TILIST)
@@ -89,6 +114,7 @@ echo 2- Create QCHEM input for Reconvergence
 echo 2a - Create Clean List
 echo 3- Create individual QCHEM input
 echo 4- Ranged QCHEM input
+echo 5- Resubmit Unsuccesful Output
 read choice1
 
 case $choice1 in 
@@ -116,6 +142,12 @@ case $choice1 in
 	4) echo -e "\nSubmit QCHEM input giving initial, final, and increment: "
 	read input
 	RangeInput ${input[@]}	
+	;;
+
+	5) echo -e "\nSubmit unsuccesful Output List "
+	read file
+	sed -i 's/.out//' $file
+	CreateFromTags $(cat $file)
 	;;
 esac
 
